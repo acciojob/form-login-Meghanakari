0b3991b3dd066b13def72076ba86584ec2b62b03
Kari Meghana <karimeghana02@gmail.com>
2025-07-15 10:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function getFormvalue(event) {
     event.preventDefault();
     let fname = document.querySelector('input[name="fname"]').value.trim();
     let lname = document.querySelector('input[name="lname"]').value.trim();
+	if (!fname || !lname) {
+        alert("Please enter both first and last names.");
+        return;
+    }
     let fullName = fname + ' ' + lname;
-    console.log(alert(fullName));
+    alert(fullName);
 	
+}
