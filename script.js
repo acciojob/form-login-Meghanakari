39561e4f97f8ec240934f6c7358ee05990447ef4
Kari Meghana <karimeghana02@gmail.com>
2025-07-15 09:53:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-function getFormvalue() {
-    //Write your code here
-	document.getElementById('myForm').addEventListener('submit', function(event) {
+function getFormvalue(event) {
     event.preventDefault();
     let fname = document.querySelector('input[name="fname"]').value.trim();
     let lname = document.querySelector('input[name="lname"]').value.trim();
     let fullName = fname + ' ' + lname;
     alert(fullName);
-});
-
 }
